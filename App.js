abce62b1781fe70e8974473c8c34113ec95d3da1
gingheea <116153792+gingheea@users.noreply.github.com>
2024-03-31 20:01:44 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,37 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import './App.css';
- function Button(props)
- {
-const handlerClick = () =>
- { 
-  props.onClickAct(props.bkColor);
-  props.onClickAct2(props.color);
- };  
-return (
-<button className="Button" onClick={handlerClick} style={{ backgroundColor: props.bkColor ,color:props.color}}>
-{props.text} 
-</button>
-);
+
+class Button extends React.Component {
+  render() {
+    const handler = () => {
+      this.props.onClickAct(this.props.bkColor);
+      this.props.onClickAct2(this.props.color);
+    };
+    return (
+      <button onClick={handler} style={{ backgroundColor: this.props.bkColor, color: this.props.color }}>
+        {this.props.text}
+      </button>
+    );
+  }
 }
-function DisplayBlock(props) 
-{
-return (
-<div  class="DisplayBlock" style={{ backgroundColor: props.bkColor,color: props.color}}> Some text </div>
-);
+
+class MyDiv extends React.Component {
+  render() {
+    return (
+      <div style={{ backgroundColor: this.props.bkColor,color:this.props.color}}>Some text</div>
+    );
+  }
 }
+
 function App() {
- const [displayBkColor, setDisplayBkColor] = useState("white");
- const [displayColor, setDisplayColor] = useState("black");
-const stateFunc = (newBkColor) => { setDisplayBkColor(newBkColor); }; 
-const stateFunc2 = (newColor) => { setDisplayColor(newColor); };  
-return (
-<>
-<Button bkColor="red" color="green" text="Red" onClickAct={stateFunc} onClickAct2={stateFunc2}/>
-<Button bkColor="green" color="red" text="Green" onClickAct={stateFunc} onClickAct2={stateFunc2}/>
-<Button bkColor="yellow" color="pink" text="Pink" onClickAct={stateFunc} onClickAct2={stateFunc2}/>
-<DisplayBlock bkColor={displayBkColor} color={displayColor}/>
-</>
-);
+  const [displayBkColor, setDisplayBkColor] = React.useState("white");
+  const [displayColor, setDisplayColor] = React.useState("black");
+
+  const stateFunc = (newBkColor) => { setDisplayBkColor(newBkColor); };
+  const stateFunc2 = (newColor) => { setDisplayColor(newColor); };
+
+  return (
+    <>
+      <Button bkColor="red" color="green" text="Red" onClickAct={stateFunc} onClickAct2={stateFunc2} />
+      <Button bkColor="green" color="red" text="Green" onClickAct={stateFunc} onClickAct2={stateFunc2} />
+      <Button bkColor="yellow" color="blue" text="Yellow" onClickAct={stateFunc} onClickAct2={stateFunc2} />
+      <MyDiv bkColor={displayBkColor} color={displayColor}/>
+    </>
+  );
 }
 
 export default App;
